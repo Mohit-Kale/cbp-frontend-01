e7d1839675e38059fc5559f175ed8e90ccfaf2a1
Mohit Kale <mohitkale.synsoft@gmail.com>
2025-11-12 14:58:46 +0530

    logged in changes

--- src/components/auth/useAuthDialog.hook.tsx
+++ src/components/auth/useAuthDialog.hook.tsx
@@ -9,12 +9,13 @@ export function useAuthDialog() {
   const dispatch = useReduxDispatch()
   const router = useRouter()
   const pathname = usePathname()
-  const { isLoggedIn } = useReduxSelector((state) => state?.auth)
+  const { isLoggedIn } = useReduxSelector((state) => state?.user)
 
   const { authDialogOpen, authMode, authRole } = useReduxSelector((state: RootState) => state?.auth?.authDialog)
 
   // opens the auth dialog and sets mode + role
   const openAuthDialog = (mode: TAuthMode, role: TRoles = 'USER') => {
+    console.log({ isLoggedIn })
     if (isLoggedIn) return
     dispatch(handleAuthDialog({ authDialogOpen: true, authMode: mode, authRole: role }))
   }
